1f0e3272b18bdb640c91cf80079b75861d5650bd
root <root@8ef07997f829>
2020-03-27 22:29:23 +0000

    yay, I am almost finished with the most annoying lesson ever!

--- src/_03_high_low/HighLowGame.java
+++ src/_03_high_low/HighLowGame.java
@@ -11,35 +11,35 @@ public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100.
-		int random = new Random().nextInt(101)+1;
+		int random = new Random().nextInt(101) + 1;
 
 		// 2. Print out the random variable above
-		//for (int a = 0; a < 10; a++) {
-			System.out.print(random);
-		//}
+
+		//System.out.print(random);
 		// 11. Repeat steps 1 to 10 ten times
 
 		// 1. Ask the user for a guess using a pop-up window, and save their response
-			for (int a = 0; a < 8; a++) {
+		for (int a = 0; a < 8; a++) {
+
 			
 			String answer = JOptionPane.showInputDialog(null, "Guess a number between 1-100! ");
-		// 4. Convert the users’ answer to an int (Integer.parseInt(string))
+			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
 			int num = Integer.parseInt(answer);
-		if (num == random) {
-			JOptionPane.showMessageDialog(null, "You won!");
-		System.exit(0);
-		}
-		if(num < random) {
-			JOptionPane.showMessageDialog(null, "Your number is too low. You lose!");
-		}
-		if(num > random) {
-			JOptionPane.showMessageDialog(null, "Your number is too high. You lose!");
-		}
+			if (num == random) {
+				JOptionPane.showMessageDialog(null, "You won!");
+				System.exit(0);
 			}
+			if (num < random) {
+				JOptionPane.showMessageDialog(null, "Your number is too low.");
+			}
+			if (num > random) {
+				JOptionPane.showMessageDialog(null, "Your number is too high.");
+			}
+		}
 		// 5. if the guess is correct
 		// 6. Win
 		// 12. Use "System.exit(0);" to quit the game if the user guessed the right
-	
+
 		// answer.
 		// 7. if the guess is high
 		// 8. Tell them it's too high
